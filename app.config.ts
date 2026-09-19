95a144028f67319c6faa0050dcb64562798c3c26
Janidu Pasan <janidupasan2@gmail.com>
2026-09-19 06:58:59 +0530

    build(ios): bump build number to 102 for UAT TestFlight release

--- app.config.ts
+++ app.config.ts
@@ -25,7 +25,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     bundleIdentifier: IS_UAT ? 'com.unexo.mytodoomobile' : 'com.mytodoo.mytodoolive',
     supportsTablet: true,
-    buildNumber: "101",
+    buildNumber: "102",
     googleServicesFile: IS_UAT
       ? './GoogleService-Info-UAT.plist'
       : './GoogleService-Info.plist',
